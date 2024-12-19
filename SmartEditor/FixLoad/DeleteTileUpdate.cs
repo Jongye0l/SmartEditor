806ab266feefee3edb2d6ab2e5aad5315b0473d2
Jongyeol <bcpjy1233@gmail.com>
2024-12-19 09:06:17 +0900

    fix: Delete is not need Change FloatDirection

--- SmartEditor/FixLoad/DeleteTileUpdate.cs
+++ SmartEditor/FixLoad/DeleteTileUpdate.cs
@@ -51,7 +51,6 @@ public class DeleteTileUpdate {
         levelMaker.listFloors.RemoveRange(floor, size);
         try {
             scrFloor prevFloor = levelMaker.listFloors[floor - 1];
-            prevFloor.floatDirection = removedFloor.floatDirection;
             if(floor == levelMaker.listFloors.Count) {
                 prevFloor.nextfloor = null;
                 prevFloor.isportal = true;
