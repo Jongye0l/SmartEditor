30652e2d2062be7a48f3cca28ef33a123f71c25e
Jongyeol <bcpjy1233@gmail.com>
2025-01-04 16:40:40 +0900

    refactor: Updated JALib Menu

--- SmartEditor/Main.cs
+++ SmartEditor/Main.cs
@@ -1,13 +1,12 @@
 ﻿using JALib.Core;
 using SmartEditor.FixLoad;
-using UnityModManagerNet;
 
 namespace SmartEditor;
 
 public class Main : JAMod {
     public static Main Instance;
 
-    public Main(UnityModManager.ModEntry modEntry) : base(modEntry, false) {
+    protected override void OnSetup() {
         AddFeature(new FixChartLoad());
     }
 
