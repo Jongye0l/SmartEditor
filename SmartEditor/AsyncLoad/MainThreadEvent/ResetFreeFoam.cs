4a7cbf40782634cfcc1d92f8cbac43435bd9f8ce
Jongyeol <bcpjy1233@gmail.com>
2025-03-05 08:43:27 +0900

    fix: FreeRoam not completed

--- SmartEditor/AsyncLoad/MainThreadEvent/ResetFreeFoam.cs
+++ SmartEditor/AsyncLoad/MainThreadEvent/ResetFreeFoam.cs
@@ -1,7 +1,12 @@
-﻿namespace SmartEditor.AsyncLoad.MainThreadEvent;
+﻿using System.Threading.Tasks;
+
+namespace SmartEditor.AsyncLoad.MainThreadEvent;
 
 public class ResetFreeFoam : ApplyMainThread {
+    public TaskCompletionSource<bool> tcs = new();
+
     public override void Run() {
         scrLevelMaker.instance.ClearFreeroam();
+        tcs.SetResult(true);
     }
 }
--- SmartEditor/AsyncLoad/Sequence/Event/FreeRoamEvent.cs
+++ SmartEditor/AsyncLoad/Sequence/Event/FreeRoamEvent.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using ADOFAI;
 using DG.Tweening;
@@ -15,49 +16,54 @@ public class FreeRoamEvent : LoadSequence {
 
     public FreeRoamEvent(SetupEvent setupEvent) {
         this.setupEvent = setupEvent;
-        setupEvent.setupEventMainThread.AddEvent(new ResetFreeFoam());
-        JATask.Run(Main.Instance, ApplyEvent);
+        ResetFreeFoam resetFreeFoam = new();
+        setupEvent.setupEventMainThread.AddEvent(resetFreeFoam);
+        resetFreeFoam.tcs.Task.GetAwaiter().UnsafeOnCompleted(ApplyEvent);
     }
 
     public void ApplyEvent() {
-        List<LevelEvent>[] floorEvents = setupEvent.floorEvents;
-        List<scrFloor> floors = scrLevelMaker.instance.listFloors;
-        List<float> floorAngles = scnGame.instance.levelData.angleData;
-        string text = Main.Instance.Localization["AsyncMapLoad.ApplyEvent4"];
-        for(;cur < floors.Count; cur++) {
-            scrFloor floor = floors[cur];
-            if(!floor.nextfloor) continue;
-            SequenceText = string.Format(text, cur, floorAngles.Count);
-            List<LevelEvent> levelEventList = floorEvents[cur];
-            List<LevelEvent> freeraomEvents = null;
-            if(levelEventList == null) continue;
-            foreach(LevelEvent levelEvent in levelEventList) {
-                if(levelEvent.eventType is < LevelEventType.FreeRoam or > LevelEventType.FreeRoamWarning) continue;
-                if(levelEvent.eventType == LevelEventType.FreeRoam) {
-                    if(levelEvent.GetInt("duration") < 2) continue;
-                    floor.freeroamRegion = freeroamRegion++;
-                    floor.freeroam = true;
-                    floor.freeroamDimensions = (Vector2) levelEvent["size"];
-                    floor.freeroamOffset = (Vector2) levelEvent["positionOffset"];
-                    int duration = levelEvent.GetInt("duration");
-                    int outTime = levelEvent.GetInt("outTime");
-                    if(outTime > duration - 1) outTime = duration - 1;
-                    floor.freeroamEndEarlyBeats = outTime;
-                    floor.freeroamEndEase = (Ease) levelEvent.data["outEase"];
-                    if(levelEvent.data.TryGetValue("hitsoundOnBeats", out object value)) floor.freeroamSoundOnBeat = (HitSound) value;
-                    if(levelEvent.data.TryGetValue("hitsoundOffBeats", out value)) floor.freeroamSoundOffBeat = (HitSound) value;
-                    floor.SetOpacity(scrController.instance.paused ? 0.1f : 0.0f);
-                    floor.opacityVal = 0.0f;
+        try {
+            Main.Instance.Log("FreeRoamEvent: Hello");
+            List<LevelEvent>[] floorEvents = setupEvent.floorEvents;
+            List<scrFloor> floors = scrLevelMaker.instance.listFloors;
+            List<float> floorAngles = scnGame.instance.levelData.angleData;
+            string text = Main.Instance.Localization["AsyncMapLoad.ApplyEvent4"];
+            for(; cur < floors.Count && cur != floorAngles.Count; cur++) {
+                SequenceText = string.Format(text, cur, floorAngles.Count);
+                scrFloor floor = floors[cur];
+                List<LevelEvent> levelEventList = floorEvents[cur];
+                List<LevelEvent> freeraomEvents = null;
+                if(levelEventList == null) continue;
+                foreach(LevelEvent levelEvent in levelEventList) {
+                    if(levelEvent.eventType is < LevelEventType.FreeRoam or > LevelEventType.FreeRoamWarning) continue;
+                    if(levelEvent.eventType == LevelEventType.FreeRoam) {
+                        if(levelEvent.GetInt("duration") < 2) continue;
+                        floor.freeroamRegion = freeroamRegion++;
+                        floor.freeroam = true;
+                        floor.freeroamDimensions = (Vector2) levelEvent["size"];
+                        floor.freeroamOffset = (Vector2) levelEvent["positionOffset"];
+                        int duration = levelEvent.GetInt("duration");
+                        int outTime = levelEvent.GetInt("outTime");
+                        if(outTime > duration - 1) outTime = duration - 1;
+                        floor.freeroamEndEarlyBeats = outTime;
+                        floor.freeroamEndEase = (Ease) levelEvent.data["outEase"];
+                        if(levelEvent.data.TryGetValue("hitsoundOnBeats", out object value)) floor.freeroamSoundOnBeat = (HitSound) value;
+                        if(levelEvent.data.TryGetValue("hitsoundOffBeats", out value)) floor.freeroamSoundOffBeat = (HitSound) value;
+                        floor.SetOpacity(scrController.instance.paused ? 0.1f : 0.0f);
+                        floor.opacityVal = 0.0f;
+                    }
+                    freeraomEvents ??= [];
+                    freeraomEvents.Add(levelEvent);
                 }
-                freeraomEvents ??= [];
-                freeraomEvents.Add(levelEvent);
+                if(freeraomEvents != null) setupEvent.setupEventMainThread.AddEvent(new ApplyFreeRoamEvent(floor, freeraomEvents));
             }
-            if(freeraomEvents != null) setupEvent.setupEventMainThread.AddEvent(new ApplyFreeRoamEvent(floor, freeraomEvents));
-        }
-        if(cur >= floorAngles.Count + 1) Dispose();
-        else {
-            SequenceText = string.Format(text, cur, floorAngles.Count);
-            Task.Yield().GetAwaiter().UnsafeOnCompleted(ApplyEvent);
+            if(cur >= floorAngles.Count) Dispose();
+            else {
+                SequenceText = string.Format(text, cur, floorAngles.Count);
+                Task.Yield().GetAwaiter().UnsafeOnCompleted(ApplyEvent);
+            }
+        } catch (Exception e) {
+            Main.Instance.LogReportException(e);
         }
     }
 
